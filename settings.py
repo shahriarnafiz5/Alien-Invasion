a489734c403483b4de81d981407c43dddf574ce0
shahriarnafiz5 <arafatshahriar512330@gmail.com>
2026-09-24 21:28:19 +0600

    Add ship_speed setting

--- settings.py
+++ settings.py
@@ -1,7 +1,2 @@
-class Settings:
-    """A class to store all settings for Alien Invasion"""
-    def __init__(self):
-        """Initialize  the game's settings"""
-        self.screen_width = 1200
-        self.screen_height= 800
-        self.bg_color = (230,230,230)
+git add settings.py
+git commit -m "Add ship_speed setting"
