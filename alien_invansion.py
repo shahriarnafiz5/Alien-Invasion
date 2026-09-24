007774471ba7e84d3dceabc642723c238c8dc0eb
shahriarnafiz5 <arafatshahriar512330@gmail.com>
2026-09-24 20:41:04 +0600

    Move ship right when right arrow key is pressed

--- alien_invansion.py
+++ alien_invansion.py
@@ -35,6 +35,11 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
 
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    #move the ship to the right 
+                    self.ship.rect.x+=1
+
 
     def _update_screen(self):
         """Update images on the screen , and flip to the new screen"""
