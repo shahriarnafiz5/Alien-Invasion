fec6af1a08386b4fbdf92954d7d3e2cbe9241374
shahriarnafiz5 <arafatshahriar512330@gmail.com>
2026-09-24 14:39:41 +0600

    Add basic AlienInvasion class with game window

---
+++ alien_invansion.py
@@ -0,0 +1,29 @@
+import sys
+import pygame
+
+class AlienInvasion:
+    """Overall class to manage game assets and behavior"""
+
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+        pygame.init()
+        self.screen = pygame.display.set_mode((1200,800))
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        """Start the main loop for the game"""
+        while True:
+            #watch for keyword and mouse event
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            #make the most recectly drawn screen visible
+
+if __name__ == '__main__':
+    #make a game instance, and run the game
+    a1 = AlienInvasion()
+    a1.run_game()
+
+
+
