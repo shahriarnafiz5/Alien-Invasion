b2a5e960a35b68d94c2bfd4c153ae0c2124889df
shahriarnafiz5 <arafatshahriar512330@gmail.com>
2026-09-24 21:11:01 +0600

    Move ship left while left arrow is held

--- alien_invansion.py
+++ alien_invansion.py
@@ -42,11 +42,15 @@ class AlienInvasion:
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = True
+                if event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
                     
 
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = False
+                if event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
 
     def _update_screen(self):
