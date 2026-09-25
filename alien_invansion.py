987a92d97a2c617b27d9fd95b4471c0492e5c819
shahriarnafiz5 <arafatshahriar512330@gmail.com>
2026-09-25 15:33:54 +0600

    Fire bullets on spacebar and update/draw them

--- alien_invansion.py
+++ alien_invansion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior"""
@@ -19,6 +20,7 @@ class AlienInvasion:
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
         
 
@@ -33,6 +35,7 @@ class AlienInvasion:
             self.ship.update()
             self._update_screen()
             self.clock.tick(60)
+            self.bullets.update()
 
     
     def _check_events(self):
@@ -58,6 +61,9 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q :
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+
 
     def _check_keyup_events(self,event):
         if event.key == pygame.K_RIGHT:
@@ -74,10 +80,15 @@ class AlienInvasion:
     def _update_screen(self):
         """Update images on the screen , and flip to the new screen"""
         self.screen.fill(self.settings.bg_color)
+        for bullet in self.bullets.sprites():
+             bullet.draw_bullet()
         self.ship.blitme()
-
         pygame.display.flip()
 
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group"""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
 
 
 
